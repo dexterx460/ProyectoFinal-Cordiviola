4bf26916f24f11b659d393646f94cbb6dd6028e8
Dexterx460 <Dexterx460@gmail.com>
2025-06-30 18:18:01 -0300

    Cambios1

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ Linkedin: Alan Exequiel Prestia
 Nombre: Octavio Cordiviola
 
 Linkedin: Octavio Cordiviola
+
+Instrucciones para su uso:
+1: Ingresar "Python manage.py runserver" en la terminal.
+2: Abrirlo en la web
+3: En el buscador ingresar "/app/Inicio/"
+4: En el header puede encontrar todos los formularios para conpletar y buscar usuarios.
+
+orden:
+1: Primero registrese y complete el formulario.
+2: Puede probar el formulario "Subir blog" o "Subir producto" en el orden que usted quiera.
+3: Por ultimo puede probar buscar usuario.
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
